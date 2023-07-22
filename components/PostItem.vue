23112ed52a3d70fb10bf7d7f7f273511dd8d6f88
presuposicionalismo <presuposicionalismoapologetica@gmail.com>
2023-07-21 22:25:23 -0500

    actualizacion del blog

--- components/PostItem.vue
+++ components/PostItem.vue
@@ -1,12 +1,4 @@
 <template>
-  <!-- <h2>
-    <NuxtLink :to="content._path">
-      {{ content.title }}
-    </NuxtLink>
-  </h2>
-  <span>{{ date }}</span>
-  <p>{{ content.description }}</p> -->
-
   <article>
 
     <div class="mx-auto">
@@ -19,29 +11,30 @@
             class="group relative sm:-mx-8 p-6 sm:p-8  bg-transparent shadow-2xl shadow-transparent sm:gap-8 sm:flex transition duration-300 hover:z-10">
             <div class="sm:w-2/6 card-image rounded-3xl overflow-hidden transition-all duration-500">
 
-              <!-- <NuxtImg provider="imagekit" :src="content.image" :alt="content.title" fit="thumb" format="webp"
-              class="scale-10' transition-all duration-400 hover:scale-100 rounded h-56 sm:h-full w-full object-cover object-top duration-500 group-hover:scale-105" /> -->
+              <NuxtImg provider="imagekit" :src="item.image" :alt="item.title" fit="thumb" format="webp"
+              class="scale-10' transition-all duration-400 hover:scale-100 rounded h-56 sm:h-full w-full object-cover object-top duration-500 group-hover:scale-105" /> -
+              
             </div>
 
             <div class="sm:p-2 sm:pl-0 sm:w-4/6">
               <span class="mt-4 mb-2 inline-block font-medium sm:mt-0 text-center md:text-left">
-                <Date :date="content.date" />
+                <Date :date="item.date" />
               </span>
-              <NuxtLink :to="content.url || content._path" :target="content.url ? '_blank' : '_self'">
+              <NuxtLink :to="item.url || item._path" :target="item.url ? '_blank' : '_self'">
 
                 <h3 class="text-2xl font-semibold text-center md:text-left">
-                  {{ content.title }}
+                  {{ item.title }}
                 </h3>
 
               </NuxtLink>
 
 
               <p class="my-6 text-justify">
-                {{ content.description }}
+                {{ item.description }}
               </p>
 
               <div class="flex gap-4 justify-center lg:justify-start">
-                <NuxtLink :to="content.url || content._path" :target="content.url ? '_blank' : '_self'">
+                <NuxtLink :to="item.url || item._path" :target="item.url ? '_blank' : '_self'">
 
                   <button
                     class="btn btn-sm btn-active btn-ghost hover:bg-base-200 hover:shadow-lg hover:shadow-base-300">Leer
@@ -59,11 +52,9 @@
 </template>
   
 <script setup lang="ts">
-import type { MarkdownParsedContent } from "@nuxt/content/dist/runtime/types";
+import type { BlogPostPreview } from '~/types'
 
-const props = defineProps<{
-  content: MarkdownParsedContent
-}>();
-
-const date = computed(() => new Date(props.content.date).toDateString());
+defineProps<{
+  item: BlogPostPreview 
+}>()
 </script>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,52 +1,34 @@
 <script setup lang="ts">
 import type { BlogPost, Sections } from "~/types";
-
 const pageSize = 4;
 
 const route = useRoute();
 const page = ref(+(route.query.page || 1));
 
 async function fetchData() {
-  const { data: articles } = await useAsyncData(() => {
-    return queryContent<BlogPost>('blog')
-      //.only(["title", "author", "date", "image", "description", "_path"])
+  const { data } = await useAsyncData(() => {
+    return queryContent<BlogPost>("blog")
+      .only(["title", "author", "date", "image", "description", "_path"])
       .sort({ date: -1 })
       .skip(pageSize * (page.value - 1))
       .limit(pageSize)
       .find();
   });
 
-  contentList.value = articles.value || [];
+  contentList.value = data.value || [];
 }
 
-const { data: articles } = await useAsyncData(() => {
-  return queryContent<BlogPost>('blog')
-    //.only(["title", "author", "date", "image", "description", "_path"])
-    .sort({ date: -1 })
-    .skip(pageSize * (page.value - 1))
-    .limit(pageSize)
-    .find();
+watch(() => route.query, (query) => {
+  page.value = +(query.page || 1)
+  fetchData();
 });
+
+const contentList = ref<any[]>([]);
+await fetchData();
 
 const title: string = "Blog Posts";
 const description: string = "Here's a list of all my blog posts";
 const section: Sections = "blog";
-
-useHead({
-  title,
-  meta: [{ name: "description", content: description }],
-});
-
-watch(
-  () => route.query,
-  (query) => {
-    page.value = +(query.page || 1);
-    fetchData();
-  }
-);
-
-const contentList = ref<any[]>([]);
-await fetchData();
 </script>
 
 <template>
@@ -54,7 +36,11 @@ await fetchData();
     <AppTitle class="pt-20">{{ title }}</AppTitle>
     <AppIntro>{{ description }}</AppIntro>
     <Tags :section="section" />
-    <ItemList v-if="articles !== null" :list="articles" :section="section" />
+
+    <div class="py-6 m-auto max-w-4xl" v-for="(item, key) of contentList">
+      <PostItem :item="item" />
+      <hr class="invisible" v-if="key !== contentList.length - 1" />
+    </div>
     <Pagination :page-size="pageSize" />
   </main>
 </template>
